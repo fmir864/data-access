8d76d32420d113b5ff4f87492a46685296d9012a
fmir864 <fmir864@outlook.com>
2018-11-06 17:17:30 +0530

    Construct the interface and the base classes for each data provider

---
+++ DataAccess/IDataProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public interface IDataProvider
+    {
+        void Connect(Dictionary<String, String> connectionOpts);
+        void Disconnect();
+        void BeginTransaction();
+        void CommitTransaction();
+        void RollbackTransaction();
+        bool CreateDatabase();
+        bool DeleteDatabase();
+        string GetConnectionString();
+
+        object GetValue(string statement, Dictionary<string, object> parameters);
+        DataSet GetDataSet(string statement, Dictionary<string, object> parameters);
+        DataTable GetDataTable(string statement, Dictionary<string, object> parameters);
+
+        bool SetDataTable(DataTable table, string tableName, string fields);
+
+        int SqlInsert(string statement, Dictionary<string, object> parameters);
+        int SqlUpdate(string statement, Dictionary<string, object> parameters);
+        int SqlDelete(string statement, Dictionary<string, object> parameters);
+        int ExecuteSqlQuery(string statement, Dictionary<string, object> parameters);
+    }
+}
---
+++ DataAccess/SqLite.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class SqLite : IDataProvider
+    {
+        public void BeginTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CommitTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Connect(Dictionary<string, string> connectionOpts)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CreateDatabase()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteDatabase()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ExecuteSqlQuery(string statement, Dictionary<string, object> parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetConnectionString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataSet GetDataSet(string statement, Dictionary<string, object> parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetDataTable(string statement, Dictionary<string, object> parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetValue(string statement, Dictionary<string, object> parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RollbackTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SetDataTable(DataTable table, string tableName, string fields)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SqlDelete(string statement, Dictionary<string, object> parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SqlInsert(string statement, Dictionary<string, object> parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SqlUpdate(string statement, Dictionary<string, object> parameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
